2a9fad8ef26f6bab99ac336105ec5989adde4b3c
martinpasco <martinpasco@hotmail.com>
2024-06-15 16:36:23 -0400

    update

--- scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-//valor de year
-var date = new Date();
-var year = date.getFullYear();
-document.getElementById("year").innerHTML = year;
-//modificaciones
-document.getElementById("demo").innerHTML = new Date(document.lastModified);
