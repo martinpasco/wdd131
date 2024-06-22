b02729c388448a2b9e3567b3692f1546fd6d77f2
martinpasco <martinpasco@hotmail.com>
2024-06-22 15:50:41 -0400

    new modifications

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+//value de year
+const date = new Date();
+const year = date.getFullYear();
+document.getElementById('curYr').innerHTML = year;
+
+const text = document.lastModified;
+document.getElementById("demo").innerHTML = text;
